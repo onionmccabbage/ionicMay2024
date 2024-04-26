4f99697df86561fe17c740ed40055d7409d3d3a2
onionmccabbage <onionmccabbage@live.com>
2024-04-26 11:29:02 +0100

    iterable content

--- myApp/src/pages/Tab1.tsx
+++ myApp/src/pages/Tab1.tsx
@@ -1,23 +1,39 @@
-import { IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { allUsers } from '../static/userData';
+import { useState } from 'react';
 
 const Tab1: React.FC = () => {
+  const [users, setUsers] = useState(allUsers)
+  const alterUsers = (e: any) => {
+    // need to add/edit users
+    setUsers([...users, e])
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Users Tab</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Tab A</IonTitle>
           </IonToolbar>
         </IonHeader>
-          <IonButtons >Go</IonButtons>       
-          {/* <ExploreContainer name="Tab 1 page" /> */}
+        <IonList>
+          {/* in React always provide an index when iterating */}
+          {users.map((user, index) => { //index=user.id
+            return <IonItem key={index}>
+              <IonLabel >{user.first_name} {user.last_name}</IonLabel>
+              <IonAvatar>
+                <img src={user.avatar} alt={user.first_name} />
+              </IonAvatar>
+            </IonItem>
+          })}
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -16,7 +16,7 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <h1>Live Auto Reload</h1>
       </IonContent>
     </IonPage>
   );
